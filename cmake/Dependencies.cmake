488eb3995cd14a958f090ce9b5833e3436578ebd
vannavolgaa <marquesasaribitrage@gmail.com>
2025-11-11 12:10:32 +0100

    modify lib cmake build

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -3,10 +3,3 @@ include(FetchContent)
 FetchContent_Declare(eigen URL https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz)
 
 FetchContent_MakeAvailable(eigen)
-
-IF (NOT TARGET uninstall)
-  ADD_CUSTOM_TARGET(uninstall "${CMAKE_COMMAND}" -P "${CMAKE_CURRENT_BINARY_DIR}/cmake_uninstall.cmake")
-  if(ENABLE_SOLUTION_FOLDERS)
-    set_target_properties(uninstall PROPERTIES FOLDER "CMakeTargets")
-  endif()
-endif()
