eabeab3f2b38697345c57b32a945157b1a0c9997
vannavolgaa <marquesasaribitrage@gmail.com>
2025-11-11 12:39:22 +0100

    change eigen version

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,5 +1,5 @@
 include(FetchContent)
 
-FetchContent_Declare(eigen URL https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz)
+FetchContent_Declare(eigen URL https://gitlab.com/libeigen/eigen/-/archive/5.0.0/eigen-5.0.0.tar.gz)
 
 FetchContent_MakeAvailable(eigen)
